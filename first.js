22560e700d9e0ce2aa6790c82bd70932bf15fce2
SolderingKnowledge <imaikol@yahoo.com>
2019-06-28 13:17:32 -0700

    data types

---
+++ first.js
@@ -0,0 +1,6 @@
+var b; //b=true;
+var n; //n=5;
+var s; //s = "some";
+var a; //a = "", true, [], {}, function();
+var arr; //arr = [];
+var ob; // ob= {name: "name"};
---
+++ first.ts
@@ -0,0 +1,9 @@
+let b: boolean;//b=true;
+let n: number; //n=5;
+let s: string; //s = "some";
+let a: any; //a = "", true, [], {}, function();
+let arr: []; //arr = [];
+let ob: {}; // ob= {name: "name"};
+
+
+
