bb93495f746a3a0945205c2f7a5b38fb9fd81b16
SolderingKnowledge <imaikol@yahoo.com>
2019-06-28 14:42:12 -0700

    logical

--- first.js
+++
@@ -1,6 +0,0 @@
-var b; //b=true;
-var n; //n=5;
-var s; //s = "some";
-var a; //a = "", true, [], {}, function();
-var arr; //arr = [];
-var ob; // ob= {name: "name"};
---
+++ logical.ts
@@ -0,0 +1,11 @@
+function returning(num: number, str: string):any{//return any data type
+    return str + num;
+}
+returning(5, 'hello'); 
+
+
+function nothing(): void{//will return null or undefined
+    return;
+}
+
+nothing();
