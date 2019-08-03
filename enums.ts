b5dbea972173e3d755feab1b1e9f0239725a5c04
Aikol Tashbolot uulu <imaikol@yahoo.com>
2019-08-03 14:52:21 -0700

    enums

---
+++ enums.ts
@@ -0,0 +1,9 @@
+//enums are cool feature to make numbers are more expressive
+
+enum Child{
+  Nick,//0 Nick=34
+  Jason,//1 Jason = 37
+  Erick//2 Erick = 40
+}
+
+let children: Child =Child.Erick;//2 and at the second example we will have 40
