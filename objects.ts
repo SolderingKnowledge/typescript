998c67943fc40d0b35498054c475c6a182d3c913
Aikol Tashbolot uulu <imaikol@yahoo.com>
2019-08-03 17:45:06 -0700

    union types

---
+++ objects.ts
@@ -0,0 +1,8 @@
+//properties names are important to identify, you can change the order but you can't change the names
+
+const obj: {a:string, b: string} = {
+  a: "a",
+  b: "b"
+}
+
+const o: {} = {}
---
+++ unionTypes.ts
@@ -0,0 +1,3 @@
+//union types allow you to specify one or more types to assign specifically
+let level: number | string = 1;
+level = "first";
